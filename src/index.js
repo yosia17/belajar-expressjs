398754433a03cf6bc59c773d9697ddbed73592e2
yosia17 <yosia_17@apps.ipb.ac.id>
2023-04-26 13:40:28 +0700

    feat: add create product

--- src/index.js
+++ src/index.js
@@ -9,9 +9,25 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 app.get('/products', async (req, res) => {
   const products = await prisma.product.findMany();
   res.send(products);
+});
+
+app.post('/products', async (req, res) => {
+  const newProductData = req.body;
+
+  const product = await prisma.product.create({
+    data: {
+      name: newProductData.name,
+      description: newProductData.description,
+      image: newProductData.image,
+      price: newProductData.price,
+    },
+  });
+  res.send('create product success');
 });
 
 app.listen(PORT, () => {
